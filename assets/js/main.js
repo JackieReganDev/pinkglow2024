c35f98052be91724c11c69847d2338815a25a174
Patrick Sullivan <patrick.sullivan@algolia.com>
2024-07-12 01:03:46 -0400

    Make JS form selector more specific

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,7 +169,7 @@
 
 			//contact form.
 
-  document.querySelector('form').addEventListener('submit', function(event) {
+  document.querySelector('form[name="contact"]').addEventListener('submit', function(event) {
     const name = document.querySelector('input[name="name"]').value;
     const email = document.querySelector('input[name="email"]').value;
     const message = document.querySelector('textarea[name="message"]').value;
